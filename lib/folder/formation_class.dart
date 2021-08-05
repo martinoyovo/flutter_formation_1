caf94b032534dee3105aed7757348cae75a71724
Martino Yovo <martinosoyen@gmail.com>
2021-08-05 10:28:34 +0200

    Fomation class created

---
+++ lib/folder/formation_class.dart
@@ -0,0 +1,13 @@
+class Formation {
+  String date = "My name";
+  String? hour;
+
+  void setDate(String new_date) {
+    date = new_date;
+    print(date);
+  }
+
+  String getDate() {
+    return date;
+  }
+}
