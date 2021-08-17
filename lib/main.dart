1829150977d940d30778b324a741a4c02278045e
infos@logexit.com <infos@logexit.com>
2021-08-17 09:41:18 +0000

    modif

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:first_app/screen/bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:first_app/folder/formation_class.dart';
+import 'package:first_app/screen/visit_card.dart';
 import 'package:first_app/screen/visit_card.dart';
 
 void main() {
@@ -7,6 +9,9 @@ void main() {
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Flutter Code Sample';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: VisitCard(),
+      home: MyStatefulWidget(),
     );
   }
 }
