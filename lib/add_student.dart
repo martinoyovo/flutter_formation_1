9ec62ea2eda2b1a3efe17cc59a8ff60e9b2a17f9
Martino Yovo <martinosoyen@gmail.com>
2021-08-24 16:23:12 +0200

    Page to add student

---
+++ lib/add_student.dart
@@ -0,0 +1,79 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class AddStudent extends StatelessWidget {
+  const AddStudent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController nomC = TextEditingController();
+    TextEditingController prenomC = TextEditingController();
+    TextEditingController ageC = TextEditingController();
+    TextEditingController noteC = TextEditingController();
+
+    ///DEFINE DATABASE COLLECTION
+    CollectionReference liste_etudiants = FirebaseFirestore.instance.collection("liste_etudiants");
+
+    Future<void> addStudent(Map<String,dynamic> student) async {
+      await liste_etudiants.add(student);
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("AJOUTER UN ETUDIANT"),
+        elevation: 0,
+      ),
+      body: Form(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          children: [
+            SizedBox(height: 20,),
+            TextFormField(
+              controller: prenomC,
+              decoration: InputDecoration(
+                labelText: "Prénom",
+                border: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).primaryColor)),
+              ),
+            ),
+            SizedBox(height: 10,),
+            TextFormField(
+              controller: nomC,
+              decoration: InputDecoration(
+                labelText: "Nom",
+                border: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).primaryColor)),
+              ),
+            ),
+            SizedBox(height: 10,),
+            TextFormField(
+              controller: ageC,
+              decoration: InputDecoration(
+                labelText: "Age",
+                border: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).primaryColor)),
+              ),
+            ),
+            SizedBox(height: 10,),
+            TextFormField(
+              controller: noteC,
+              decoration: InputDecoration(
+                labelText: "Note",
+                border: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).primaryColor)),
+              ),
+            ),
+            SizedBox(height: 20,),
+            ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Theme.of(context).primaryColor)
+                ),
+                onPressed: () {
+                  addStudent(
+                      {
+
+                      });
+                },
+                child: Text("Ajouter")
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
