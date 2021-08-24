6e95d621d6b0bb5ec26f70ae31df32f599a97109
Martino Yovo <martinosoyen@gmail.com>
2021-08-24 16:16:04 +0200

    Route name added

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:first_app/students_list.dart';
 import 'package:first_app/widgets/text_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:first_app/folder/formation_class.dart';
 
-void main() {
-
+void main() async {
   runApp(MyApp());
+  await Firebase.initializeApp();
 }
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Gestion Etudiants',
       theme: ThemeData(
         primaryColor: Colors.pink,
+        accentColor: Colors.pink
       ),
       home: StudentsList(),
     );
@@ -24,7 +26,7 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: textWidgets()
+      body: StudentsList()
     );
   }
 }
