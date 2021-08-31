60ae50b1c9decb121444864b56596116ce729387
Martino Yovo <martinosoyen@gmail.com>
2021-08-31 10:30:19 +0200

    List of students from firebase

--- lib/firebase_project/students_list.dart
+++ lib/firebase_project/students_list.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:first_app/firebase_project/add_student.dart';
 import 'package:first_app/widgets/custom_card.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +8,7 @@ class StudentsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    CollectionReference liste_etudiants = FirebaseFirestore.instance.collection("liste_etudiants");
 
     return Scaffold(
       appBar: AppBar(
@@ -23,13 +25,41 @@ class StudentsList extends StatelessWidget {
           ));
         },
       ),
-      body: ListView(
+      body: FutureBuilder(
+        future: liste_etudiants.get(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot<Object?>> snapshot) {
+          if(snapshot.hasData) {
+            print(snapshot.data!.docs.length);
+            return ListView.builder(
+              padding: EdgeInsets.only(top: 15),
+              itemCount: snapshot.data!.docs.length,
+                itemBuilder: (BuildContext context, int index) {
+                final document = snapshot.data!.docs[index];
+                  return CustomCard(studentInfos: document);
+                });
+          }
+          else if(snapshot.hasError) {
+
+          }
+          else if(snapshot.connectionState == ConnectionState.waiting) {
+
+          }
+
+          else {
+
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      )
+      /*ListView(
         padding: EdgeInsets.only(top: 15),
         children: [
           CustomCard(),
           CustomCard(),
         ],
-      ),
+      ),*/
     );
   }
 }
