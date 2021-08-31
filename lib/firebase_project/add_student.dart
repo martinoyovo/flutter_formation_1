86134e9cda4a38429977d47f64d698c176d8466b
Martino Yovo <martinosoyen@gmail.com>
2021-08-31 12:51:25 +0200

    TextFormField validator, Form globalKey, Form state validation, Showing snackbar

--- lib/firebase_project/add_student.dart
+++ lib/firebase_project/add_student.dart
@@ -17,12 +17,15 @@ class AddStudent extends StatelessWidget {
     Future<void> addStudent(Map<String,dynamic> student) async {
       await liste_etudiants.add(student);
     }
+    ///Validation Key du formulaire d'ajout de l'étudiant
+    GlobalKey<FormState> _formKey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(
         title: Text("AJOUTER UN ETUDIANT"),
         elevation: 0,
       ),
       body: Form(
+        key: _formKey,
         child: ListView(
           padding: EdgeInsets.symmetric(horizontal: 10),
           children: [
@@ -33,6 +36,11 @@ class AddStudent extends StatelessWidget {
                 labelText: "Prénom",
                 border: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).primaryColor)),
               ),
+              validator: (value) {
+                if(value!.length < 3) {
+                  return "Entrer un prénom valide";
+                }
+              },
             ),
             SizedBox(height: 10,),
             TextFormField(
@@ -41,22 +49,40 @@ class AddStudent extends StatelessWidget {
                 labelText: "Nom",
                 border: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).primaryColor)),
               ),
+              validator: (value) {
+                if(value!.length < 3) {
+                  return "Entrer un nom valide";
+                }
+              },
             ),
             SizedBox(height: 10,),
             TextFormField(
               controller: ageC,
+              ///CHANGER LE TYPE DE CLAVER/AJOUTER UN CLAVIER DE CHIFFRE
+              keyboardType: TextInputType.number,
               decoration: InputDecoration(
                 labelText: "Age",
                 border: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).primaryColor)),
               ),
+              validator: (value) {
+                if(value!.length < 2) {
+                  return "L'age n'est pas valide";
+                }
+              },
             ),
             SizedBox(height: 10,),
             TextFormField(
               controller: noteC,
+              keyboardType: TextInputType.number,
               decoration: InputDecoration(
                 labelText: "Note",
                 border: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).primaryColor)),
               ),
+              validator: (value) {
+                if(value!.length < 2) {
+                  return "La note n'est pas valide";
+                }
+              },
             ),
             SizedBox(height: 20,),
             ElevatedButton(
@@ -67,13 +93,27 @@ class AddStudent extends StatelessWidget {
                     backgroundColor: MaterialStateProperty.all(Theme.of(context).primaryColor)
                 ),
                 onPressed: () {
-                  addStudent(
-                      {
-                        "nom": nomC.text,
-                        "prenom": prenomC.text,
-                        "age": ageC.text,
-                        "note": noteC.text
-                      });
+                  if(_formKey.currentState!.validate()) {
+                    addStudent(
+                        {
+                          "nom": nomC.text,
+                          "prenom": prenomC.text,
+                          "age": ageC.text,
+                          "note": noteC.text
+                        }).then((value) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text("Etudiant ajouté avec succès."), backgroundColor: Colors.pink,)
+                      );
+                      Navigator.pop(context);
+                    });
+
+                  }
+                  else {
+                    ///AFFICHER UN SNACKBAR/ALERTE
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text("Tous les champs ne sont pas remplis."), backgroundColor: Colors.pink,)
+                    );
+                  }
                 },
                 child: Text("Ajouter", style: TextStyle(fontSize: 17),)
             )
