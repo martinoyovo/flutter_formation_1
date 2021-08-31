e7f70a972635dd0e1c2e90148cc1e69c5b59f205
Martino Yovo <martinosoyen@gmail.com>
2021-08-31 13:18:15 +0200

    Adding edit student route to the list

--- lib/widgets/custom_card.dart
+++ lib/widgets/custom_card.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:first_app/firebase_project/edit_student.dart';
 import 'package:flutter/material.dart';
 
 
@@ -9,41 +10,44 @@ class CustomCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final primaryColor = Theme.of(context).primaryColor;
-    return Card(
-      color: Colors.grey.shade300,
-      margin: EdgeInsets.only(left: 15, right: 15, bottom: 8,),
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10)
-      ),
-      child: Row(
-        children: [
-          Container(
-              height: 88,
-              width: 80,
-              decoration: BoxDecoration(
-                  color: primaryColor,
-                  borderRadius: BorderRadius.horizontal(left: Radius.circular(10))
-              )
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(studentInfos["prenom"]+studentInfos["nom"], style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                SizedBox(height: 3,),
-                Text("${studentInfos["age"]} ans", style: TextStyle(fontSize: 15),),
-                Row(
-                  children: [
-                    Icon(Icons.assignment_rounded, color: primaryColor),
-                    SizedBox(width: 5,),
-                    Text("Note: ${studentInfos["note"]}"),
-                  ],
+    return GestureDetector(
+      onTap: () => Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => EditStudent(documentID: studentInfos.id))),
+      child: Card(
+        color: Colors.grey.shade300,
+        margin: EdgeInsets.only(left: 15, right: 15, bottom: 8,),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10)
+        ),
+        child: Row(
+          children: [
+            Container(
+                height: 88,
+                width: 80,
+                decoration: BoxDecoration(
+                    color: primaryColor,
+                    borderRadius: BorderRadius.horizontal(left: Radius.circular(10))
                 )
-              ],
             ),
-          )
-        ],
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(studentInfos["prenom"]+' '+studentInfos["nom"], style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                  SizedBox(height: 3,),
+                  Text("${studentInfos["age"]} ans", style: TextStyle(fontSize: 15),),
+                  Row(
+                    children: [
+                      Icon(Icons.assignment_rounded, color: primaryColor),
+                      SizedBox(width: 5,),
+                      Text("Note: ${studentInfos["note"]}"),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
